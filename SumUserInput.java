2a1bdd0409de4317aec8c0ee50196ad4c96da70c
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-07-07 09:41:54 +0530

    Add files via upload

---
+++ SumUserInput.java
@@ -0,0 +1,13 @@
+//Write a program to sum three numbers in Java(user input)
+package question_solution;
+import java.util.Scanner;
+public class SumUserInput {
+    public static void main(String[] args){
+        System.out.println("Enter the three number:-");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        System.out.println("The sum of "+a+"+"+b+"+"+c+"= "+(a+b+c));
+    }
+}
