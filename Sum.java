c5bf308e2f122baf3397c9a63f21561dc6a4a5eb
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-07-07 09:40:42 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,12 @@
+//Write a program to sum three numbers in Java
+package question_solution;
+public class Sum {
+    public static void main(String[]args){
+        int num1=5,num2=8,num3=3,s;
+        s=num1+num2+num3;
+        System.out.print("Sum=");
+        System.out.println(s);
+        System.out.println("The sum of the number is :");
+        System.out.println(s);
+    }
+}
