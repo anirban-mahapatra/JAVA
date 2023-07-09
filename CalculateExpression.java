fed3880185802253e9f15bffb1cbecc9334bf5f3
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-07-09 11:49:27 +0530

    Add files via upload

---
+++ CalculateExpression.java
@@ -0,0 +1,11 @@
+//CALCULATE X=++Y*8
+package question_solution;
+import java.util.Scanner;
+public class CalculateExpression {
+    public static void main(String[] arg){
+     Scanner sc=new Scanner(System.in);
+     System.out.print("Enter the value of y:-");
+     int y=sc.nextInt();
+     System.out.println("x="+(++y *8));
+    }
+}
