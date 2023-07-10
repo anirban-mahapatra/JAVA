db2d7e0ded1f0f87286ea9605724bf7f9ac949b6
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-07-10 22:27:55 +0530

    Add files via upload

---
+++ GraterOrNot.java
@@ -0,0 +1,13 @@
+//USE COMPARISON OPERATOR TO FIND WHETHER A GIVEN NUMBER IS GRATER THEN USER ENTERED NUMBER OR NOT
+package question_solution;
+import java.util.Scanner;
+public class GraterOrNot {
+    public static void main(String[] arg){
+        Scanner sc=new Scanner(System.in);
+        int b=10;
+        System.out.print("Enter a number:-");
+        int a=sc.nextInt();
+        System.out.println(a+" is grater then "+b+"\t\t"+(a>10));
+    }
+
+}
