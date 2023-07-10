169172115994e190ecb736356dbdf6f252c07c32
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-07-10 22:27:38 +0530

    Add files via upload

---
+++ EvaluateExpression1.java
@@ -0,0 +1,8 @@
+//EVALUATE EXPRESSION A=7/4 * 9/2
+package question_solution;
+public class EvaluateExpression1 {
+    public static void main(String[] arg){
+        float a=7/4f * 9/2f;
+        System.out.println(a);
+    }
+}
