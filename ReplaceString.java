f225c05def40a7776ff95207a9b3c527e918abfe
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-07-12 21:08:09 +0530

    Add files via upload

---
+++ ReplaceString.java
@@ -0,0 +1,17 @@
+/*
+WRITE A JAVA PROGRAM TO FILL IN A LETTER TEMPLATE WHICH LOOK LIKE BELOW:
+    LETTER = "DEAR <|NAME|>, THANKS A LOT"
+    REPLACE <|NAME|> WITH A STRING
+ */
+package question_solution;
+import java.util.Scanner;
+public class ReplaceString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String letter="\"DEAR <|NAME|>, THANKS A LOT\"";
+        System.out.print("\"DEAR <|NAME|>, THANKS A LOT\"\nEnter the name you want to replace with <|NAME|> :-");
+        String a=sc.nextLine();
+        String name=a.toUpperCase();
+        System.out.println("\n"+letter.replace("<|NAME|>",name));
+    }
+}
