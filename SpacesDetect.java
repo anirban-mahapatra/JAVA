e3ab03002a9f592034325f4a725a5b3433c738f3
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-07-26 13:18:56 +0530

    Add files via upload

---
+++ SpacesDetect.java
@@ -0,0 +1,13 @@
+//WRITE A JAVA PROGRAM TO DETECT DOUBLE AND TRIPLE SPACES IN A STRING
+package question_solution;
+import java.util.Scanner;
+public class SpacesDetect {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter any string with double and triple spaces:-");
+        String a=sc.nextLine();
+        System.out.println(a.length());
+        System.out.println("Double spaces in index no:- "+a.indexOf("  "));
+        System.out.println("Triple spaces in index no:- "+a.indexOf("   "));
+    }
+}
