49efc1d378f9d5490e4933b0b52cee62a2c002b4
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-07-27 16:53:12 +0530

    Add files via upload

---
+++ WebsiteTypes.java
@@ -0,0 +1,28 @@
+/*
+WRITE A PROGRAM TO FIND OUT THE TYPES OF WEBSITE FROM THE URL
+    .com= COMMERCIAL WEBSITE
+    .org= ORGANISATION WEBSITE
+    .in= INDIAN WEBSITE
+ */
+package question_solution;
+import java.util.Scanner;
+public class WebsiteTypes {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the URL:-");
+        String u=sc.next();
+        String url=u.toLowerCase();
+        if(url.endsWith(".com")){
+            System.out.println("\n"+url+" is a Commercial Website");
+        }
+        else if(url.endsWith(".org")){
+            System.out.println("\n"+url+" is a Organisation Website");
+        }
+        else if(url.endsWith(".in")){
+            System.out.println("\n"+url+" is a Indian Website");
+        }
+        else{
+            System.out.println("\nWrong URL");
+        }
+    }
+}
