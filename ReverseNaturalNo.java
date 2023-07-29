f15d56b977c543250c9dcd79608ac5ec0223028f
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-07-29 21:01:14 +0530

    Add files via upload

---
+++ ReverseNaturalNo.java
@@ -0,0 +1,14 @@
+//WRITE A JAVA PROGRAM TO PRINT FIRST N NATURAL NUMBER IN REVERSE NUMBER.
+package question_solution;
+import java.util.Scanner;
+public class ReverseNaturalNo {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of natural number:-");
+        int n=sc.nextInt();
+        System.out.print("The natural number are:- ");
+        for(int i=n;i>0;i--){
+            System.out.println(i);
+        }
+    }
+}
