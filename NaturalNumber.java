a38783f564ab0d60cc0b1c5462b642585ba5a6b1
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-07-29 21:01:43 +0530

    Add files via upload

---
+++ NaturalNumber.java
@@ -0,0 +1,13 @@
+//WRITE A JAVA PROGRAM TO PRINT NATURAL NUMBER FROM 100 TO 200.
+package question_solution;
+
+public class NaturalNumber {
+    public static void main(String[]args){
+        int i=100;
+        System.out.print("The natural number from 100 to 200:- ");
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
