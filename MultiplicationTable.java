1ade7e9c4f94ae529d8bb0f13a73b4be55eb8adc
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-07-31 11:21:04 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,14 @@
+//WRITE A PROGRAM TO PRINT MULTIPLICATION TABLE OF A GIVER NUMBER OF N
+package question_solution;
+import java.util.Scanner;
+public class MultiplicationTable {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the multiplication table number:- ");
+        int mul=sc.nextInt();
+        System.out.println("\nThe multiplication table of "+mul+" is:-");
+        for(int i=1;i<=10;i++){
+            System.out.println(mul+" * "+i+" = "+(mul*i));
+        }
+    }
+}
