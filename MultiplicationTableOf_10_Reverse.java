ce19e53e21e3a837a0ca5c9d6f7d6d5f03437c26
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-07-31 11:21:20 +0530

    Add files via upload

---
+++ MultiplicationTableOf_10_Reverse.java
@@ -0,0 +1,10 @@
+//WRITE A PROGRAM TO PRINT MULTIPLICATION TABLE OF 10 IN REVERSE ORDER
+package question_solution;
+public class MultiplicationTableOf_10_Reverse {
+    public static void main(String[]args){
+        System.out.println("The multiplication table of 10 on reverse order:- ");
+        for(int i=10;i>0;i--){
+            System.out.println("10 * "+i+" = "+(10*i));
+        }
+    }
+}
