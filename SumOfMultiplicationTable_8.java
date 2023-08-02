e1397de6900bd27769d76c3dcdb29527b30e878d
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-08-02 14:05:26 +0530

    Add files via upload

---
+++ SumOfMultiplicationTable_8.java
@@ -0,0 +1,11 @@
+//WRITE A PROGRAM TO CALCULATE THE SUM OF THE NUMBER OCCURRING MULTIPLICATION TABLE OF 8
+package question_solution;
+public class SumOfMultiplicationTable_8 {
+    public static void main(String[]args){
+        int sum=0;
+        for(int i=1;i<=10;i++){
+            sum+=(8*i);
+        }
+        System.out.println("The sum of multiplication table of 8 is "+sum);
+    }
+}
