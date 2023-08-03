2c3e181ebe76a458f71cd53547c522e1be419a74
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-08-03 18:44:17 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,18 @@
+//WRITE A JAVA PROGRAM TO PRINT A ARRAY
+package question_solution;
+import java.util.Scanner;
+public class Array {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int []arr=new int[10];
+        System.out.print("Enter the number of element:- ");
+        int n=sc.nextInt();
+        System.out.println("Enter the elements:- ");
+        for(int i=0;i<n;i++) {
+            arr[i]= sc.nextInt();
+        }
+        for(int j=0;j<n;j++){
+            System.out.print(arr[j]+"\t");
+        }
+    }
+}
