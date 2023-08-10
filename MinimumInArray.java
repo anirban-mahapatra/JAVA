f31ad9546d8759505d89950c3544a8b597195bff
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-08-10 21:36:26 +0530

    Add files via upload

---
+++ MinimumInArray.java
@@ -0,0 +1,22 @@
+//WRITE A JAVA PROGRAM TO FOND THE MINIMUM ELEMENT IN ARRAY.
+package question_solution;
+import java.util.Scanner;
+public class MinimumInArray {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int []arr=new int[10];
+        System.out.print("Enter the number of element:- ");
+        int n=sc.nextInt();
+        System.out.println("Enter the elements:- ");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int min=Integer.MAX_VALUE;
+        for(int j=0;j<n;j++){
+            if(arr[j]<min){
+                min=arr[j];
+            }
+        }
+        System.out.println("The min value of the array is "+min);
+    }
+}
