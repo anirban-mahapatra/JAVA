744e06d829c7104eed96bc1197862f8ecef6790e
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-09-06 18:20:24 +0530

    Add files via upload

---
+++ SumOfEvenNumberUsingWhile.java
@@ -0,0 +1,23 @@
+//WRITE A JAVA PROGRAM TO SUM FIRST N NUMBER OF EVAN NUMBER USING While.
+package question_solution;
+import java.util.Scanner;
+public class SumOfEvenNumberUsingWhile {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of even number:- ");
+        int  n=sc.nextInt();
+
+
+//      USING WHILE LOOP
+        int even=0;
+        int i=0;
+        System.out.print("\nThe even number are:- ");
+        while(i<n){
+            System.out.print(2*i+"\t");
+            even=even+(2*i);
+            i++;
+        }
+        System.out.println("\n\nThe sum of even numbers is= "+even);
+
+    }
+}
