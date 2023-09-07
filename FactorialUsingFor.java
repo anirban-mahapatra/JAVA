8e70c54921ee21cc0a3da44b841bd8452d3b0cbb
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-09-07 19:19:51 +0530

    Add files via upload

---
+++ FactorialUsingFor.java
@@ -0,0 +1,17 @@
+//WRITE A PROGRAM TO FIND THE FACTORIAL OF A GIVER NUMBER USING FOR LOOP.
+package question_solution;
+import java.util.Scanner;
+public class FactorialUsingFor {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number to calculate factorial:- ");
+        int n=sc.nextInt();
+        int fac=1;
+
+//      USING FOR
+        for(int i=n;i>0;i--){
+            fac=fac*i;
+        }
+        System.out.println("\nThe factorial of "+n+" is "+fac);
+    }
+}
