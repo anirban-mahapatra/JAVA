03eb3248c7d66bfece5338a4156e97cabec342ef
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-09-09 09:40:41 +0530

    Add files via upload

---
+++ PatternUsingFor.java
@@ -0,0 +1,26 @@
+/*
+WRITE A JAVA PROGRAM TO PRINT FOLLOWING PATTERN USING FOR:
+****
+***
+**
+*
+
+ */
+
+package question_solution;
+import java.util.Scanner;
+public class PatternUsingFor {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of * print in a row:- ");
+        int n=sc.nextInt();
+
+        for(int i=0;i<n;i++){
+            for(int j=n-i;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
