9c60370531280f92d20fb75309a68519c50cd50f
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-09-14 12:11:09 +0530

    Add files via upload

---
+++ PatternUsingMethod.java
@@ -0,0 +1,25 @@
+/*WRITE A PROGRAM USING METHOD TO PRINT THE FOLLOWING PATTERN
+*
+**
+***
+****
+*****
+ */
+package question_solution;
+import java.util.*;
+public class PatternUsingMethod {
+    static void pattern(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of lines: ");
+        int num=sc.nextInt();
+        pattern(num);
+    }
+}
