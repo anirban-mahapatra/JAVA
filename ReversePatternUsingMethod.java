b31bdec478a9e503c1bf36ac69bf9a6aad446fe6
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-09-15 13:37:50 +0530

    Add files via upload

---
+++ ReversePatternUsingMethod.java
@@ -0,0 +1,25 @@
+/*WRITE A PROGRAM USING METHOD TO PRINT THE FOLLOWING PATTERN
+ *****
+ ****
+ ***
+ **
+ *
+ */
+package question_solution;
+import java.util.*;
+public class ReversePatternUsingMethod {
+    static void reverse(int n){
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of line: ");
+        int n=sc.nextInt();
+        reverse(n);
+    }
+}
