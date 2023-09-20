10203fceda494a77dbe4b60ce5e1375547ec0e16
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-09-20 19:04:31 +0530

    Add files via upload

---
+++ PatternUsingRecursion1.java
@@ -0,0 +1,26 @@
+/*WRITE A PROGRAM USING METHOD TO PRINT THE FOLLOWING PATTERN USING RECURSION
+ *
+ **
+ ***
+ ****
+ *****
+ */
+package question_solution;
+import java.util.*;
+public class PatternUsingRecursion1 {
+    static void pattern(int n){
+        if(n>0){
+            pattern(n-1);
+            for(int i=0;i<n;i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of row:- ");
+        int n=sc.nextInt();
+        pattern(n);
+    }
+}
