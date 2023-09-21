e0de42944c0e766e10d4cbb0763d631b256936b9
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-09-21 09:19:21 +0530

    Add files via upload

---
+++ ReversPatternUsingRecursion1.java
@@ -0,0 +1,27 @@
+/*WRITE A PROGRAM USING METHOD TO PRINT THE FOLLOWING PATTERN USING RECURSION.
+ *****
+ ****
+ ***
+ **
+ *
+ */
+package question_solution;
+import java.util.Scanner;
+public class ReversPatternUsingRecursion1 {
+    static void pattern(int n){
+        if(n>0){
+            for(int i=0;i<n;i++){
+                System.out.print("*");
+            }
+            System.out.println();
+            pattern(n-1);
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of row:- ");
+        int n=sc.nextInt();
+        pattern(n);
+    }
+}
+
